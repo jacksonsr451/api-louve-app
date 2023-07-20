c77e9b639761c2d24a3170e00ba4c49851f1543f
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-19 22:35:50 -0300

    refactor: update class and add geters and seters

--- src/Domain/MinistryMembersManagement/Member.php
+++ src/Domain/MinistryMembersManagement/Member.php
@@ -4,11 +4,57 @@ namespace App\Domain\MinistryMembersManagement;
 
 class Member
 {
-    public function __construct(
-        public readonly string $id,
-        public readonly string $name,
-        public readonly array $skills,
-        public readonly array $availability,
-    ) {
+
+    private string $id;
+    private string $name;
+    private array $skills;
+    private array $availability;
+
+    public function __construct(string $id, string | null $name, array | null $skills, array | null $availability)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->skills = $skills;
+        $this->availability = $availability;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getSkills(): array
+    {
+        return $this->skills;
+    }
+
+    public function getAvailability(): array
+    {
+        return $this->availability;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setSkills(array $skills): void
+    {
+        $this->skills = $skills;
+    }
+
+    public function setAvailability(array $availability): void
+    {
+        $this->availability = $availability;
     }
 }
