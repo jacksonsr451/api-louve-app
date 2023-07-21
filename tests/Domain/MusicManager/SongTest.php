56fd6b20f090d2e6dc6d7af5cca387560d683a15
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-21 08:24:53 -0300

    test: include test to get ministerID in SongTest::class

--- tests/Domain/MusicManager/SongTest.php
+++ tests/Domain/MusicManager/SongTest.php
@@ -9,25 +9,31 @@ class SongTest extends TestCase
 {
     public function testGetTitle()
     {
-        $song = new Song('Song Title', 'https://example.com', 'Song lyrics...', 'Song chords...');
+        $song = new Song('Song Title', '1', 'https://example.com', 'Song lyrics...', 'Song chords...');
         $this->assertEquals('Song Title', $song->getTitle());
+    }
+
+    public function testGetMinisterID()
+    {
+        $song = new Song('Song Title', '1', 'https://example.com', 'Song lyrics...', 'Song chords...');
+        $this->assertEquals('1', $song->getMinisterID());
     }
 
     public function testGetLink()
     {
-        $song = new Song('Song Title', 'https://example.com', 'Song lyrics...', 'Song chords...');
+        $song = new Song('Song Title', '1', 'https://example.com', 'Song lyrics...', 'Song chords...');
         $this->assertEquals('https://example.com', $song->getLink());
     }
 
     public function testGetLyrics()
     {
-        $song = new Song('Song Title', 'https://example.com', 'Song lyrics...', 'Song chords...');
+        $song = new Song('Song Title', '1', 'https://example.com', 'Song lyrics...', 'Song chords...');
         $this->assertEquals('Song lyrics...', $song->getLyrics());
     }
 
     public function testGetChords()
     {
-        $song = new Song('Song Title', 'https://example.com', 'Song lyrics...', 'Song chords...');
+        $song = new Song('Song Title', '1', 'https://example.com', 'Song lyrics...', 'Song chords...');
         $this->assertEquals('Song chords...', $song->getChords());
     }
 }
