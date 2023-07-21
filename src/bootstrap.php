f339615068721e33b1b95caf6e2a9236e782ef91
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-21 19:32:15 -0300

    chore: alter locals to init routes and add init in dotenv by bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
+use Jacksonsr45\RadiantPHP\Http\Request;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/routes.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$router->handleRequest(new Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']));
