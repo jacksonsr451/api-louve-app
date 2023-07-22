8e8704ba21954b7c76a4e111821137d858347e1b
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-22 11:59:46 -0300

    refactor: repace to SontDTO type in getMusicByTitle function

--- src/Application/MusicManager/Contracts/MusicManagerServiceIntrerface.php
+++ src/Application/MusicManager/Contracts/MusicManagerServiceIntrerface.php
@@ -7,6 +7,6 @@ interface MusicManagerServiceInterface
     public function addMusic(SongDTO $song): void;
     public function deleteMusic(SongDTO $song): void;
     public function getMusics(): array;
-    public function getMusicByTitle(string $title): SongDTO;
+    public function getMusicByTitle(SongDTO $song): SongDTO;
     public function getMusicByMinister(SongDTO $song): array;
 }
