a390884fb159bf717b92a7bae7ae6921a33b89df
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-27 09:43:52 -0300

    chore: update jacksonsr45/radiante-php and setting default configs

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
 use Dotenv\Dotenv;
-use Jacksonsr45\RadiantPHP\Http\Request;
+use Jacksonsr45\RadiantPHP\ServerRequestFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/routes.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$router->handleRequest(new Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']));
+$pathToRoute = __DIR__ . '/routes.php';
+$request = ServerRequestFactory::createServerRequest($pathToRoute);
+$response = ServerRequestFactory::handleRequest($request);
+ServerRequestFactory::sendHttpResponse($response);
